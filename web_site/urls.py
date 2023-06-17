f5f263bbbfb10cb861584befd6f3c4c4888247c9
Danich000 <d.bayutin000@mail.ru>
2023-06-17 17:26:32 +0300

    modificatiom

--- web_site/urls.py
+++ web_site/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Limoner project.
+URL configuration for web_site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
